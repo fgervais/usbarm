27ba9484f853d625bb6be9ecac82dd2b2eeed2ca
FrancoisGervais <FrancoisGervais@4b0b7f4c-ae1d-11de-ab15-2d6928411550>
2009-10-02 14:32:49 +0000

    Worked on Gpio framework

--- trunk/Gpio.cpp
+++ trunk/Gpio.cpp
@@ -7,11 +7,28 @@
 
 #include "Gpio.h"
 
-Gpio::Gpio() {
-	// TODO Auto-generated constructor stub
+#include "stm32f10x.h"
+#include "GpioPin.h"
+#include "GpioConfiguration.h"
 
+/**
+ * @brief Gpio constructor.
+ *
+ * This function takes care of the individual pins initialization.
+ */
+Gpio::Gpio() {
+	uint32_t flag = 1;
+	for(uint8_t i=0; i<16; i++) {
+		// Create a new pin with it's corresponding flag.
+		gpioPins[i] = new GpioPin(this, flag);
+		flag <<= 1;
+	}
 }
 
 Gpio::~Gpio() {
 	// TODO Auto-generated destructor stub
+}
+
+void Gpio::configure(GpioConfiguration config) {
+
 }
---
+++ trunk/GpioConfiguration.cpp
@@ -0,0 +1,17 @@
+/*
+ * GpioConfiguration.cpp
+ *
+ *  Created on: Oct 2, 2009
+ *      Author: fgervais
+ */
+
+#include "GpioConfiguration.h"
+
+GpioConfiguration::GpioConfiguration() {
+	// TODO Auto-generated constructor stub
+
+}
+
+GpioConfiguration::~GpioConfiguration() {
+	// TODO Auto-generated destructor stub
+}
