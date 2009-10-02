913db9133aadb338b4260113b98d6ccee9f736c2
FrancoisGervais <FrancoisGervais@4b0b7f4c-ae1d-11de-ab15-2d6928411550>
2009-10-02 00:39:49 +0000

    Makefile has been modified so all the code (including the C code) is compiled with G++. This should avoid some problems in the future. STM32F103 class is the actual implementation of the mcu we are using. This should have been included in the previous subversion.

---
+++ STM32F103.cpp
@@ -0,0 +1,17 @@
+/*
+ * STM32F103.cpp
+ *
+ *  Created on: Oct 1, 2009
+ *      Author: fgervais
+ */
+
+#include "STM32F103.h"
+
+STM32F103::STM32F103() {
+	// TODO Auto-generated constructor stub
+
+}
+
+STM32F103::~STM32F103() {
+	// TODO Auto-generated destructor stub
+}
