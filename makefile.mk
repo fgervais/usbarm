bce61bc0186178625886f697ebf48868a0f8e076
FrancoisGervais <FrancoisGervais@4b0b7f4c-ae1d-11de-ab15-2d6928411550>
2009-10-03 03:34:38 +0000

    Started HAL

--- makefile.mk
+++ makefile.mk
@@ -61,7 +61,7 @@ SRCARM  =
 
 # List C++ source files here.
 # use file-extension cpp for C++-files (use extension .cpp)
-CPPSRC = $(wildcard *.cpp)
+CPPSRC = $(wildcard *.cpp) $(wildcard ./Drivers/*.cpp)
 
 # List C++ source files here which must be compiled in ARM-Mode.
 # use file-extension cpp for C++-files (use extension .cpp)
