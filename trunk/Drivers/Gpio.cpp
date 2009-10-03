4de26ed733db7e8d4d84fe6515d1602fa7732789
FrancoisGervais <FrancoisGervais@4b0b7f4c-ae1d-11de-ab15-2d6928411550>
2009-10-03 03:35:32 +0000

    First hal driver

---
+++ trunk/Drivers/Gpio.cpp
@@ -0,0 +1,21 @@
+/*
+ * Gpio.cpp
+ *
+ *  Created on: Oct 2, 2009
+ *      Author: fgervais
+ */
+
+#include "Gpio.h"
+
+namespace Drivers {
+
+Gpio::Gpio() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Gpio::~Gpio() {
+	// TODO Auto-generated destructor stub
+}
+
+}
