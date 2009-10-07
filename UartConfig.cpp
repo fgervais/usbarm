c9bf71b2d65f5803b8f98c44d1be5e0804570abe
Remz.Oexal <Remz.Oexal@4b0b7f4c-ae1d-11de-ab15-2d6928411550>
2009-10-07 00:45:27 +0000

    add UartConfig class

---
+++ UartConfig.cpp
@@ -0,0 +1,17 @@
+/*
+ * UartConfig.cpp
+ *
+ *  Created on: 2009-10-06
+ *      Author: oex
+ */
+
+#include "UartConfig.h"
+
+UartConfig::UartConfig() {
+	// TODO Auto-generated constructor stub
+
+}
+
+UartConfig::~UartConfig() {
+	// TODO Auto-generated destructor stub
+}
