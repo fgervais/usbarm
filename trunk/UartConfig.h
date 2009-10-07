a3c9c62699ab87cbe6b07ca31b7a99a71a4d704f
Remz.Oexal <Remz.Oexal@4b0b7f4c-ae1d-11de-ab15-2d6928411550>
2009-10-07 00:45:27 +0000

    add UartConfig class

---
+++ trunk/UartConfig.h
@@ -0,0 +1,17 @@
+/*
+ * UartConfig.h
+ *
+ *  Created on: 2009-10-06
+ *      Author: oex
+ */
+
+#ifndef UARTCONFIG_H_
+#define UARTCONFIG_H_
+
+class UartConfig {
+public:
+	UartConfig();
+	virtual ~UartConfig();
+};
+
+#endif /* UARTCONFIG_H_ */
