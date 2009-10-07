0560fd8e767270d143cc719423594eee0525b583
Remz.Oexal <Remz.Oexal@4b0b7f4c-ae1d-11de-ab15-2d6928411550>
2009-10-07 00:44:51 +0000

    commencer la config du uart

--- Uart.h
+++ Uart.h
@@ -12,6 +12,21 @@
 
 class Uart: public Peripheral {
 public:
+
+	//                fck = 2 MHz
+	//  baudrate =  ---------------
+	//              (16 * USARTDIV)
+
+	static const uint8_t UART_BAUDRATE_4800		= ;	/** Uart Speed 4800 baud/s */
+	static const uint8_t UART_BAUDRATE_9600		= ;	/** Uart Speed 9600 baud/s */
+	static const uint8_t UART_BAUDRATE_19200	= ;	/** Uart Speed 19200 baud/s */
+	static const uint8_t UART_BAUDRATE_38400	= ;	/** Uart Speed 38400 baud/s */
+	static const uint8_t UART_BAUDRATE_115200	= ;	/** Uart Speed 115200 baud/s */
+
+
+
+
+
 	Uart();
 	virtual ~Uart();
 
