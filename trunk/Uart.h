25b07f362eb9cc403aa34010bb6ff93e5d6b576c
Remz.Oexal <Remz.Oexal@4b0b7f4c-ae1d-11de-ab15-2d6928411550>
2009-10-11 17:37:47 +0000

    USART is now auto-detecting the system clock and is able to transmit character chain in polling mode

--- trunk/Uart.h
+++ trunk/Uart.h
@@ -11,35 +11,15 @@
 #include "Peripheral.h"
 #include "stm32f10x.h"
 
+#include <iostream>
+#include <queue>
+
+
+
 class UartConfiguration;
 
 class Uart: public Peripheral {
 public:
-
-	/**              fck = 72000000
-	    baudrate =  ----------------
-	                 (16 * USARTDIV)
-	*/
-
-	/*	USARTDIV = 937.5, Fraction = 16 * 0.5 = 8 = 0x8,
-		Mantisse = 937 = 0x3A9, BRR = 0x00003A9C  */
-	static const uint32_t UART_BAUDRATE_4800		= 0x00003A9C;	/** Uart Speed 4800 baud/s */
-
-	/* USARTDIV = 468.75,  Fraction = 16 * 0.75 = 12 = 0xC,
-	   Mantisse = 468 = 0x1D4, BRR = 0x000001D4C  */
-	static const uint32_t UART_BAUDRATE_9600		= 0x000001D4C;	/** Uart Speed 9600 baud/s */
-
-	/* USARTDIV = 234.375, Fraction = 16 * 0.75 = 12 = 0xC,
-	   Mantisse = 234 = 0xEA, BRR = 0x00000EAC */
-	static const uint32_t UART_BAUDRATE_19200		= 0x00000EAC;	/** Uart Speed 19200 baud/s */
-
-	/* USARTDIV = 117.188, Fraction = 16 * 0.188 = 3 = 0x3,
-	 * Mantisse = 117 = 0x75, BRR = 0x00000753  */
-	static const uint32_t UART_BAUDRATE_38400		= 0x00000753;	/** Uart Speed 38400 baud/s */
-
-	/* USARTDIV = 39.0625, Fraction = 16 * 0.0625 = 1 = 0x1,
-	   Mantisse = 39 = 0x3, BRR = 0x00000271  */
-	static const uint32_t UART_BAUDRATE_115200		= 0x00000271;	/** Uart Speed 115200 baud/s */
 
 	static const uint32_t UART_WORD_LENGTH_8BIT		= 0x00000000;	/** Uart Word Lenght of 8 bit */
 	static const uint32_t UART_WORD_LENGTH_9BIT		= 0x00001000;	/** Uart Word Lenght of 9 bit  */
@@ -64,15 +44,22 @@ public:
 	static const uint32_t UART_RX_DISABLE	 		= 0x00000000;	/** UART Disabled */
 	static const uint32_t UART_RX_ENABLE	 		= 0x00000004;	/** UART Enabled */
 
+	static const uint32_t UART_TX_DATA_REGISTER		= 0x00000080; /** UART Transmission
+																data register is empty */
+
 private:
 	USART_TypeDef *uartRegisters;
+	uint32_t clockFrequency;
+	uint16_t calculateBRR(uint16_t baudrate);
+	std::queue<int8_t> charBuffer;
 
 public:
-	Uart(USART_TypeDef *uartRegisters);
+
+	Uart(USART_TypeDef *uartRegisters, uint32_t PCLK1_Frequency);
 	virtual ~Uart();
 
 	void configure(UartConfiguration config);
-	void write(char data, uint8_t lenght);
+	void write(char* data, uint16_t lenght);
 	char read(uint8_t lenght);
 	void sendInterrupt();
 	void receiveInterrupt();
