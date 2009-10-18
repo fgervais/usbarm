8951f784bf3c53fc26d55ab364ee18a6535080cc
FrancoisGervais <FrancoisGervais@4b0b7f4c-ae1d-11de-ab15-2d6928411550>
2009-10-18 17:49:02 +0000

    Template the the MAX3421E functions.

--- trunk/MAX3421E.h
+++ trunk/MAX3421E.h
@@ -8,10 +8,17 @@
 #ifndef MAX3421E_H_
 #define MAX3421E_H_
 
+#include <stdint.h>
+
 class MAX3421E {
 public:
 	MAX3421E();
 	virtual ~MAX3421E();
+
+	void writeRegister(uint8_t address, uint8_t data);
+	void writeBytes(uint8_t address, uint8_t *data, uint8_t length);
+	uint8_t readRegister(uint8_t address);
+	uint8_t readBytes(uint8_t address, uint8_t *data, uint8_t length);
 };
 
 #endif /* MAX3421E_H_ */
