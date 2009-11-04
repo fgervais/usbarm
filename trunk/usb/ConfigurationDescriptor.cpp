5298d108ca202ba3f42c9a9ca1dc4592b23d658f
FrancoisGervais <FrancoisGervais@4b0b7f4c-ae1d-11de-ab15-2d6928411550>
2009-11-04 17:23:04 +0000

    Some more descriptor added

--- trunk/usb/ConfigurationDescriptor.cpp
+++ trunk/usb/ConfigurationDescriptor.cpp
@@ -7,8 +7,15 @@
 
 #include "ConfigurationDescriptor.h"
 
-ConfigurationDescriptor::ConfigurationDescriptor() {
-
+ConfigurationDescriptor::ConfigurationDescriptor(uint8_t* rawDescriptor) {
+	length				= rawDescriptor[0];
+	descriptorType		= rawDescriptor[1];
+	totalLength			= (rawDescriptor[3] << 8) | rawDescriptor[2];
+	numInterface		= rawDescriptor[4];
+	configurationValue	= rawDescriptor[5];
+	configuration		= rawDescriptor[6];
+	attributes			= rawDescriptor[7];
+	maxPower			= rawDescriptor[8];
 }
 
 ConfigurationDescriptor::~ConfigurationDescriptor() {
---
+++ trunk/usb/InterfaceDescriptor.cpp
@@ -0,0 +1,17 @@
+/*
+ * InterfaceDescriptor.cpp
+ *
+ *  Created on: Nov 4, 2009
+ *      Author: fgervais
+ */
+
+#include "InterfaceDescriptor.h"
+
+InterfaceDescriptor::InterfaceDescriptor(uint8_t* rawDescriptor) {
+
+
+}
+
+InterfaceDescriptor::~InterfaceDescriptor() {
+
+}
