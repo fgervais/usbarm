a098f5f0e223c6826b314d3264aa7620d7907556
Remz.Oexal <Remz.Oexal@4b0b7f4c-ae1d-11de-ab15-2d6928411550>
2009-11-04 15:24:30 +0000

    remove OutputStreamCommand from UML, remove enum that link peripheral to a external device

--- trunk/CommandWriter.h
+++ trunk/CommandWriter.h
@@ -9,11 +9,14 @@
 #define COMMANDWRITER_H_
 
 #include "PeripheralEventListener.h"
+#include "CommandReceiver.h"
 
 class CommandWriter: public PeripheralEventListener {
 public:
 	CommandWriter();
 	virtual ~CommandWriter();
+private:
+	CommandReceiver *output;
 };
 
 #endif /* COMMANDWRITER_H_ */
